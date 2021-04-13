91a90a28cf550cf7d1b24c307f64357dba915f12
4696trpg <82348868+4696trpg@users.noreply.github.com>
2021-04-13 09:23:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,7 @@
 
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = traceback.format_exception(type(orig_error), orig_error, orig_error.__traceback__)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
